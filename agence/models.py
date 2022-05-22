fa866ead6977228a7cfb7447999cdf3621d3073b
Mohamed <mohamed-siso@hotmail.fr>
2022-05-22 18:30:20 +0200

    database parameters rm sqlite3 db

--- agence/models.py
+++ agence/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Client(models.Model):
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    tel = models.CharField(max_length=30)
