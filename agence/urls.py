ed6a798fb83b6b6330c87a3f18121c44f1963e01
Mohamed <mohamed-siso@hotmail.fr>
2022-05-23 15:17:50 +0200

    urls

--- agence/urls.py
+++ agence/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('agence/', include('agence.urls'))
+    path('', views.index, name='index')
 ]
