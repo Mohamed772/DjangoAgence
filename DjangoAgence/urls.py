8f05baaff021bc11aa1642447f5e48152210b78d
Mohamed <mohamed-siso@hotmail.fr>
2022-06-04 17:51:24 +0200

    V1 of models.py

--- DjangoAgence/urls.py
+++ DjangoAgence/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agence/', include('agence.urls')),
-    path('',)
+    path('agence/', include('agence.urls'))
 ]
